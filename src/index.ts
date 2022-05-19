6ee0150d38be3ec3b1e42a809b544bb1a5efe34c
Sergey Batukov <sashachepuha@gmail.com>
2022-05-19 14:01:26 +0300

    add vuex

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
-interface App {
-  directive: (
-    name: string,
-    cb: (el: HTMLElement, binding: { value: string }) => void
-  ) => void;
+import { App } from "vue";
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
+import { ThemeModule, IThemeState } from "./themeModule";
+export interface IState {
+  theme: IThemeState;
 }
+export interface IOptions {
+  vuex?: Store<Record<any, any>>;
+  themes?: {
+    [key: string]: {
+      [key: string]: string;
+    };
+  };
+}
+
+export const key: InjectionKey<Store<IState>> = Symbol();
+export const themeStore = createStore<IState>({
+  modules: {
+    VTHEME: ThemeModule,
+  },
+});
+
+export default {
+  install(Vue: App, options: IOptions) {
+    if (options?.vuex) {
+      options.vuex.registerModule("VTHEME", ThemeModule);
+    } else {
+      Vue.use(themeStore, key);
+    }
+  },
+};
+
